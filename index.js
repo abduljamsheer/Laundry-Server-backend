dc5458184936f9732928fe34f5465c074062794c
abduljamsheer <abduljamsheerh7@gmail.com>
2025-05-25 21:21:01 +0530

    modified env

--- index.js
+++ index.js
@@ -7,8 +7,8 @@ const connection = require("./database/db.js");
 const PORT = process.env.PORT || 3001;
 
 const corsOptions = {
-  origin: 'http://localhost:3000', // Allow frontend
-  credentials: true,               // Support cookies if needed
+  origin: 'http://localhost:3000', 
+  credentials: true,               
 };
 app.use(cors(corsOptions));
 app.use(express.json());
