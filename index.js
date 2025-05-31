4ae551c283649346e3876ec70ec4e8bb8cfa2db2
abduljamsheer <abduljamsheerh7@gmail.com>
2025-05-31 17:07:42 +0530

    updated

--- index.js
+++ index.js
@@ -5,22 +5,25 @@ const cors = require("cors");
 const connection = require("./database/db.js");
 
 const PORT = process.env.PORT || 3001;
-const allowedOrigins = process.env.ALLOWED_ORIGINS?.split(',') || [
-  'http://localhost:3000',
-  'https://gregarious-crumble-a31b6f.netlify.app',
-   'https://683ae7a478f84c0d3c780b5c--meek-malabi-0b01d6.netlify.app'
-];
-const corsOptions = {
-  origin: function (origin, callback) {
-    if (!origin || allowedOrigins.includes(origin)) {
-      callback(null, true);
-    } else {
-      callback(new Error('Not allowed by CORS: ' + origin));
-    }
-  },
-  credentials: true,
-};
-app.use(cors(corsOptions));
+// const allowedOrigins = process.env.ALLOWED_ORIGINS?.split(',') || [
+//   'http://localhost:3000',
+//   'https://gregarious-crumble-a31b6f.netlify.app',
+//    'https://683ae7a478f84c0d3c780b5c--meek-malabi-0b01d6.netlify.app'
+// ];
+// const corsOptions = {
+//   origin: function (origin, callback) {
+//     if (!origin || allowedOrigins.includes(origin)) {
+//       callback(null, true);
+//     } else {
+//       callback(new Error('Not allowed by CORS: ' + origin));
+//     }
+//   },
+//   credentials: true,
+// };
+// app.use(cors(corsOptions));
+app.use(cors({
+  origin: '*'
+}));
 // app.use(cors());
 // app.use(cors({
 //   origin: "http://localhost:3000",
