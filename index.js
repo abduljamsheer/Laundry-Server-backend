06374ba7022cf0d06d14c5c63953449b1cc35cd7
abduljamsheer <abduljamsheerh7@gmail.com>
2025-05-31 17:01:44 +0530

    updated

--- index.js
+++ index.js
@@ -7,7 +7,8 @@ const connection = require("./database/db.js");
 const PORT = process.env.PORT || 3001;
 const allowedOrigins = process.env.ALLOWED_ORIGINS?.split(',') || [
   'http://localhost:3000',
-  'https://gregarious-crumble-a31b6f.netlify.app'
+  'https://gregarious-crumble-a31b6f.netlify.app',
+   'https://683ae7a478f84c0d3c780b5c--meek-malabi-0b01d6.netlify.app'
 ];
 const corsOptions = {
   origin: function (origin, callback) {
@@ -20,11 +21,11 @@ const corsOptions = {
   credentials: true,
 };
 app.use(cors(corsOptions));
-app.use(cors());
-app.use(cors({
-  origin: "http://localhost:3000",
-  credentials: true
-}));
+// app.use(cors());
+// app.use(cors({
+//   origin: "http://localhost:3000",
+//   credentials: true
+// }));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
